01b503a970c70f22169550a7992b8732533a9870
Mukhammad Karimov <tafu.karimov@mail.ru>
2015-07-06 19:27:46 +0500

    initial sync

---
+++ app/src/main/java/uz/inha/muhammad/coffeetable/MainActivity.java
@@ -0,0 +1,34 @@
+package uz.inha.muhammad.coffeetable;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_number);
+        quantityTextView.setText("" + number);
+    }
+}
