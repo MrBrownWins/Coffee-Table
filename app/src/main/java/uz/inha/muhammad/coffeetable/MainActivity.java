9f01010402b90434f4c5aee8840d1be716230034
Mukhammad Karimov <tafu.karimov@mail.ru>
2015-07-07 19:20:44 +0500

    Adding dynamic functionalities

--- app/src/main/java/uz/inha/muhammad/coffeetable/MainActivity.java
+++ app/src/main/java/uz/inha/muhammad/coffeetable/MainActivity.java
@@ -3,24 +3,45 @@ package uz.inha.muhammad.coffeetable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends ActionBarActivity {
 
+    int numCoffeeOrdered;
+    int COFFEE_PRICE = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numCoffeeOrdered = 0;
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        displayPrice(numCoffeeOrdered*COFFEE_PRICE);
+    }
+    public void increaseQuantity(View view){
+        if (numCoffeeOrdered != 10) {
+            numCoffeeOrdered++;
+        }
+        display(numCoffeeOrdered);
+        //not finished
+    }
+
+    public void decreaseQuantity(View view){
+        if (numCoffeeOrdered != 0){
+            numCoffeeOrdered--;
+        }
+        display(numCoffeeOrdered);
+        //not finished
     }
 
     /**
@@ -30,5 +51,12 @@ public class MainActivity extends ActionBarActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_number);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
